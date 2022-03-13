9f739a408e9b3a1f68b5b659d66e728a435545fb
Maratkarimov21 <marat.karimov@flatstack.com>
2022-03-13 22:37:42 +0300

    modal new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,7 +9,30 @@ class TasksController < ApplicationController
   def show
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      respond_to do |format|
+        format.turbo_stream do
+          flash[:success] = 'Success'
+        end
+      end
+    else
+      render :new, flash[:error] = @task.errors
+    end
+
+  end
+
   private
+
+  def task_params
+    params.require(:task).permit(:title, :description).merge(requester: current_user)
+  end
 
   def fetch_task
     @task = Task.find_by!(id: params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'tasks#index'
 
-  resources :tasks, only: %i[index show]
+  resources :tasks, only: %i[index show new create]
 end
